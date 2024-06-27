c09b375ab2253fc0582fb16ddbd6861a7b21ad0b
dinarith.duch <dinarith.duch@cogetel.com.kh>
2024-06-27 09:05:34 +0700

    Check Update

---
+++ config/config.py
@@ -0,0 +1,7 @@
+import os
+
+APP_NAME = "Upload File FastAPI"
+APP_DESCRIPTION = "Upload File FastAPI"
+APP_VERSION = "1.0.0"
+
+IMAGE_FOLDER = ""
---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from fastapi import FastAPI, status
+
+from config.config import APP_DESCRIPTION, APP_NAME, APP_VERSION
+from config.response import SuccessfulResponseModel
+
+app = FastAPI(
+    title=APP_NAME,
+    description=APP_DESCRIPTION,
+    version=APP_VERSION
+)
+
+
+@app.get("/", status_code=status.HTTP_200_OK, tags=["default".upper()])
+def read_root():
+    return SuccessfulResponseModel(message="API is running...")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="debug")
+
